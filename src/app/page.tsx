9b0d275d555c34011b7d38ef3298387b7b083ffb
juanRCoder <guillecarck@gmail.com>
2025-01-17 18:42:21 -0500

    feat: agregar interfaz principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import { Typography } from "@mui/material";
+import { LayoutDashboard } from 'lucide-react';
+import TitleBar from "@/components/TitleBar";
+import QuarterlyFinance from '@/components/dashboard/QuarterlyFinance';
 
 export default function Dashboard() {
+  // para poner bloques uno debajo de otros juan, debes colocar dentro de una caja, 2 cajas y darle flex-col con item-start asi haras un efecto mezclandose. asi evitaras el hueco que deja
   return (
-    <Typography>Dashboard</Typography>
+    <section className='px-8 pt-8 flex flex-col gap-16'>
+      <TitleBar Icon={LayoutDashboard} title='Dashboard' />
+      <article className='inline-flex gap-6 flex-wrap items-start'>
+        <div className="px-6 py-4 rounded-md border-2 border-bg_200">
+          <p className="text-lg font-semibold">NAME RESTAURANT</p>
+        </div>
+        <QuarterlyFinance />
+      </article>
+    </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        accent_200: '#ffe49a',
         primary_200: '#c2402a',
         bg_100: '#1A1A1A',
         bg_200: '#292929',
         bg_300: "#404040",
+        cst_red: '#ec6767',
+        cst_green: '#99f694'
         // --primary-100:#8B0000;
         // --primary-200:#c2402a;
         // --primary-300:#ffa282;
         // --accent-100:#FF4500;
-        // --accent-200:#ffe49a;
         // --text-100:#FFFFFF;
         // --text-200:#e0e0e0;
       }
